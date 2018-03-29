a6016ad332a8b38b9d0d945bc913973e1da1cc74
Vijay Vishwakarma <vijayv@mail.usf.edu>
2018-03-29 19:03:06 -0400

    Create VoteCountReducer.java

---
+++ VoteCountReducer.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.util.Iterator;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+@SuppressWarnings("unused")
+public class VoteCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+	
+	@Override
+	public void reduce(Text key, Iterable<IntWritable> values, Context output)
+			throws IOException, InterruptedException {
+		int voteCount = 0;
+		for(IntWritable value: values){
+			voteCount+= value.get();
+		}
+		output.write(key, new IntWritable(voteCount));
+	}
+}
